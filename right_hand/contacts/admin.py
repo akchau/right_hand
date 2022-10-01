21151a2bec252d5f1d999e04634cadc81b653ef2
akcaau <gllazrv@gmail.com>
2022-10-01 18:58:18 +0300

    Добавлена модель компании

--- right_hand/contacts/admin.py
+++ right_hand/contacts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Contact, Communication
+from .models import Contact, Communication, Company
 
 admin.site.register(Contact)
 admin.site.register(Communication)
+admin.site.register(Company)
--- right_hand/contacts/models.py
+++ right_hand/contacts/models.py
@@ -54,3 +54,34 @@ class Communication(models.Model):
         null=True,
         default="Нет описания",
     )
+
+
+class Company(models.Model):
+    """Модель компании."""
+    inn = models.CharField("ИНН", max_length=10)
+    kpp = models.CharField("КПП", max_length=9)
+    ogrn = models.CharField("ОГРН", max_length=13)
+    okpo = models.CharField("ОКПО", max_length=10, blank=True, null=True)
+    okved = models.CharField("ОКВЭД", max_length=10)
+    fact_adress = models.TextField()
+    legal_adress = models.TextField()
+    full_name = models.CharField(
+        "Полное наименование",
+        max_length=200,
+    )
+    short_name = models.CharField(
+        "Сокращенное наименование",
+        max_length=150,
+    )
+    main_email = models.EmailField("Основной email")
+    phone_number = models.CharField("Номер телефона", max_length=30)
+    number_acount = models.CharField(
+        "Расчетный счет",
+        max_length=20,
+    )
+    cor_acount = models.CharField(
+        "Корреспондентский счет",
+        max_length=20,
+    )
+    bic = models.CharField("БИК", max_length=9)
+    head_of_company = models.CharField("ФИО руководителя", max_length=200)
