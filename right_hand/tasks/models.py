ebf83d1b197d6cd2a6c493949428363625ec5bc8
akcaau <gllazrv@gmail.com>
2022-10-11 21:22:04 +0300

    Добавлены модели Задачи, Проекта и Проекта с контактом

--- right_hand/tasks/models.py
+++ right_hand/tasks/models.py
@@ -2,17 +2,88 @@ from datetime import datetime
 
 from django.db import models
 
+from contacts.models import Company, Contact
+
+PROJECT_STATUS = [
+    ('Запланирован', 'Запланирован'),
+    ('В работе', 'В работе'),
+]
+
+
+class Project(models.Model):
+    """Модель проекта."""
+    name = models.CharField(
+        "Название проекта.",
+        max_length=200,
+        help_text="Название проекта.",
+    )
+    revenue = models.IntegerField(
+        "Выручка от проекта.",
+        help_text="Укажите выручку.",
+    )
+    deadline = models.DateTimeField(
+        "Дедлайн.",
+        help_text="Дедлайн.",
+    )
+    my_role = models.CharField(
+        "Роль в проекте.",
+        max_length=200,
+        help_text="Роль в проекте.",
+    )
+    customer = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        related_name="projects",
+        verbose_name="Заказчик",
+        help_text="Укажите заказчика.",
+        null=True,
+    )
+    status = models.CharField(
+        "Статус проекта",
+        choices=PROJECT_STATUS,
+        max_length=200,
+        help_text="Статус проекта.",
+    )
+
+    class Meta:
+        ordering = ("deadline",)
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+    @property
+    def is_expired(self):
+        if datetime.today().timestamp() > self.deadline.timestamp():
+            return True
+        return False
+
+    def __str__(self):
+        return {self.name}
+
 
 class Task(models.Model):
+    """Модель задачи."""
     name = models.CharField(
         "Тескт задачи",
         max_length=200,
         help_text="Что нужно сделать.",
     )
     deadline = models.DateTimeField(
-        "Планируемая дата коммуникации.",
-        help_text="Планируемая дата коммуникации.",
+        "Дедлайн задачи.",
+        help_text="Дедлайн задачи.",
     )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        verbose_name="Проект",
+        help_text="Укажите к какому проекту относится задача.",
+        null=True,
+    )
+
+    class Meta:
+        ordering = ("deadline",)
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
 
     @property
     def is_expired(self):
@@ -22,3 +93,29 @@ class Task(models.Model):
 
     def __str__(self):
         return f"Задача - {self.name} - Дедлайн - {self.deadline}"
+
+
+class ProjectContact(models.Model):
+    """Класс устанавливающий связь проекта с контактами."""
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="contact_project",
+        verbose_name="Проект",
+        help_text="Укажите проект контакта."
+    )
+    contact = models.ForeignKey(
+        Contact,
+        on_delete=models.CASCADE,
+        related_name="contact_project",
+        verbose_name="Контакт",
+        help_text="Укажите контакт проекта."
+    )
+    role = models.CharField(
+        "Роль в проекте.",
+        max_length=200,
+        help_text="Роль в проекте.",
+    )
+
+    def __str__(self):
+        return f"{self.project} - {self.contact}"
