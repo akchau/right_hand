c3fe167d355678835673d6004ee14687500210e4
akcaau <gllazrv@gmail.com>
2022-12-12 18:41:46 +0300

    Настроена загрузка SECRET_KEY из переменной окружения

--- right_hand/right_hand/settings.py
+++ right_hand/right_hand/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 
+from dotenv import load_dotenv
+
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uqqys24lmw!9+u_udcvlgvde!dk650_4y0sh3c^s6_^io6ecdv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
