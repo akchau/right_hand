370093fdea69362dd4fc36de6916af692c685afe
akcaau <gllazrv@gmail.com>
2022-12-15 19:12:03 +0300

    SECRET_KEY

--- right_hand/right_hand/settings.py
+++ right_hand/right_hand/settings.py
@@ -25,7 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+def get_secret_key():
+    if os.getenv('SECRET_KEY'):
+        return os.getenv('SECRET_KEY')
+    else:
+        return 'iynqjlk6($(5!*nu%m$v^5y&uvt1hjrw-y#wwqu=qlb-uyedb-'
+
+
+SECRET_KEY = get_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
